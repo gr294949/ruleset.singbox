7d39f6da9e7c34ff1b7448363d31c7c75012effe
SukkaBot <githubbot@skk.moe>
2024-11-05 12:37:28 +0000

    deploy: SukkaW/Surge@39714be0b5fe8ab2350797deb9833c6929438c70

--- Internal/appprofile.php
+++ Internal/appprofile.php
@@ -1,6 +1,6 @@
 <?php
 
-// # Build 2024-11-05T08:40:51.689Z
+// # Build 2024-11-05T12:36:55.777Z
 
 declare(strict_types=1);
 
@@ -1635,6 +1635,7 @@ $_ENV['Clash_Group_Config'] = [
         'DOMAIN-SUFFIX,cnn.com,Global',
         'DOMAIN-SUFFIX,fc2.com,Global',
         'DOMAIN-SUFFIX,hbg.com,Global',
+        'DOMAIN-SUFFIX,ibm.com,Global',
         'DOMAIN-SUFFIX,kik.com,Global',
         'DOMAIN-SUFFIX,nyt.com,Global',
         'DOMAIN-SUFFIX,okx.com,Global',
@@ -2918,7 +2919,6 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,45.157.70.0/23,Domestic',
         'IP-CIDR,45.192.64.0/19,Domestic',
         'IP-CIDR,45.199.167.0/24,Domestic',
-        'IP-CIDR,45.204.112.0/20,Domestic',
         'IP-CIDR,45.248.8.0/22,Domestic',
         'IP-CIDR,45.248.110.0/24,Domestic',
         'IP-CIDR,45.249.212.0/22,Domestic',
@@ -5324,7 +5324,6 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,154.205.112.0/21,Domestic',
         'IP-CIDR,154.205.124.0/22,Domestic',
         'IP-CIDR,154.211.72.0/24,Domestic',
-        'IP-CIDR,154.223.96.0/19,Domestic',
         'IP-CIDR,155.126.176.0/23,Domestic',
         'IP-CIDR,156.0.92.0/22,Domestic',
         'IP-CIDR,156.107.160.0/24,Domestic',
@@ -5338,8 +5337,7 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,156.232.13.0/24,Domestic',
         'IP-CIDR,156.236.119.0/24,Domestic',
         'IP-CIDR,156.239.0.0/20,Domestic',
-        'IP-CIDR,156.239.64.0/18,Domestic',
-        'IP-CIDR,156.239.224.0/19,Domestic',
+        'IP-CIDR,156.239.96.0/19,Domestic',
         'IP-CIDR,156.247.8.0/22,Domestic',
         'IP-CIDR,156.247.12.0/23,Domestic',
         'IP-CIDR,156.247.14.0/24,Domestic',
